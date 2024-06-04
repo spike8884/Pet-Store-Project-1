ae82946fb1ee028ca0ba5919a89674a1bafeccfa
Richard <spike8884@gmail.com>
2024-06-04 06:06:10 -0400

    pet store done

--- app.js
+++ app.js
@@ -12,112 +12,74 @@ var shop = [
     title: 'GHOST',
     image: './sale pics/035bec_eaddc9d0658a466499cd804f9e7f53d5~mv2.webp',
     price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur.'
-  },
-  {
-    title: 'BO & LUKE',
-    image: './sale pics/0_NCra6TruPYCn1OcG.jpg',
-    price: '',
-    description: 'Lorem ipsum dolor sit.'
+    description: 'Very lovable and watchs everything'
   },
   {
     title: 'THING',
     image: './sale pics/10-majestic-maine-coon.jpg',
     price: '',
-    description: 'Lorem ipsum dolor sit amet.'
+    description: 'Very dark and majestic and relaxed'
   },
   {
     title: 'WATCH',
     image: './sale pics/IMG_7029_JPG.webp',
     price: '',
-    description: 'Lorem ipsum dolor sit.'
+    description: 'Loves to play and wrestle and chase all over'
   },
   {
     title: 'SIMBA & TIGER',
     image: './sale pics/6-147.webp',
     price: '',
-    description: 'Lorem ipsum dolor sit.'
+    description: 'inseperable best friends always together'
   },
   {
     title: 'SKULL',
     image: './sale pics/maine-coon-striped-cat.webp',
     price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur.'
+    description: 'always hiding and pouncing'
   },
   {
     title: 'STRETCH',
     image: './sale pics/802c7ac038cc57852cccd2bf0e1dbd08.jpg',
     price: '',
-    description: 'Lorem ipsum dolor sit.'
+    description: 'lazyboy alwayys finds comfortable place for nap'
   },
   {
     title: 'MR BIG',
     image: './sale pics/900_HH-Photography-of-Florida_MC781.jpg',
     price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    description: 'very long beautiful coat loves being brushed'
   },
   {
     title: 'SMOKEY & SHADOW',
     image: './sale pics/attachment-Maine-Coon-Cat-Twix-and-Twizzle-bros-rescued-from-Brooklyn-in-Biddeford.jpg',
     price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
+    description: 'the twins are full of mischief'
   },
   {
     title: 'FLUFF & CHUNK',
     image: './sale pics/mainecoon_of_ig_81897006_118203266355720_1610196586135730123_n-e1578685230120.jpg',
-    price: '1350',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+    price: '',
+    description: 'always on the hunt no matter the weather'
   },
   {
     title: 'SPIKE',
     image: './sale pics/Maine Coon.2.jpg',
     price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+    description: 'everything is new and must be explored'
   },
   {
     title: 'CHASE',
     image: './sale pics/c3f8714ad343b4aaa1d9d9d9bca0e547.jpg',
     price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+    description: 'always on the run quick as lightning'
   },
   {
     title: 'HEATHCLIFF',
     image: './sale pics/Orange-Maine-Coon.webp',
     price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+    description: 'lapcat always wants to cuddle up'
   },
-  {
-    title: 'FURBALL',
-    image: './sale pics/cat.png',
-    price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
-  },
-  {
-    title: 'LYNX',
-    image: './sale pics/Tabbies-with-White.jpg',
-    price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
-  },
-  {
-    title: 'WILDMAN',
-    image: './sale pics/The-Maine-Coon.jpg',
-    price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
-  },
-  {
-    title: 'SAREES',
-    image: './sale pics/how-smart-is-a-maine-coon-cat-featured.jpg',
-    price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
-  },
-  {
-    title: 'BIG GUY',
-    image: './sale pics/Why-Is-My-Maine-Coon-Vibrating-1200cp.jpeg',
-    price: '',
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
-  }
-
-
 ]
 
 var postHTML = " ";
